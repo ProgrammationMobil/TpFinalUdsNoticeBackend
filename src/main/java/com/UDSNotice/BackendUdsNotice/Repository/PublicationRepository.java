1b6777026514b31ee4c85edacc9289469f462ca9
franklin <ftsowemoo2@gmail.com>
2025-05-26 07:45:01 +0100

    Création d'une interface qui étend les propriétés du JPA et qui servira communiquer avec la base de données

--- src/main/java/com/UDSNotice/BackendUdsNotice/Repository/PublicationRepository.java
+++ src/main/java/com/UDSNotice/BackendUdsNotice/Repository/PublicationRepository.java
@@ -4,5 +4,4 @@ import com.UDSNotice.BackendUdsNotice.models.Publication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PublicationRepository extends JpaRepository<Publication, Long> {
-
 }
