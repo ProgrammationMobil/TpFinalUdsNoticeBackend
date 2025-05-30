5538a19ae4c0f36b61cc4a33967f7136ef686636
franklin <ftsowemoo2@gmail.com>
2025-05-30 11:56:07 +0100

    Ajout d'une exception personnalisée pour une notification non trouvé

---
+++ src/main/java/com/UDSNotice/BackendUdsNotice/Exception/NotificationNotFound.java
@@ -0,0 +1,7 @@
+package com.UDSNotice.BackendUdsNotice.Exception;
+
+public class NotificationNotFound extends RuntimeException {
+    public NotificationNotFound(String message) {
+        super(message);
+    }
+}
